8b85189f4e00e49d59da504b7f60fb59c8e2457e
ByteNinja <pijayblog@gmail.com>
2025-09-25 14:52:57 +0700

    Delete examples directory

--- examples/send_message.py
+++
@@ -1,9 +0,0 @@
-from telegram_wrapper.client import TelegramClient
-
-# Masukkan token dari BotFather
-TOKEN = "123456789:ABC-Your-Telegram-Bot-Token"
-CHAT_ID = 123456789  # Ganti dengan chat_id kamu
-
-client = TelegramClient(TOKEN)
-response = client.send_message(CHAT_ID, "Hello from my Python SDK!")
-print(response)
